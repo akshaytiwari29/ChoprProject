d39d02613d79076bd0d5e82db31f1a2d0ce4d043
akshaytiwari29 <41627550+akshaytiwari29@users.noreply.github.com>
2018-09-06 10:25:08 +0530

    Update LibertyServlet.java

--- src/main/java/application/servlet/LibertyServlet.java
+++ src/main/java/application/servlet/LibertyServlet.java
@@ -16,32 +16,62 @@
 package application.servlet;
 
 import java.io.IOException;
-
+import java.io.PrintWriter;
+ 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-@WebServlet(urlPatterns="/servlet")
-public class LibertyServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-
+ 
+/**
+* Servlet implementation class Calculator
+*/
+@WebServlet("/Calculator")
+public class Calculator extends HttpServlet {
+                private static final long serialVersionUID = 1L;
+       
     /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     * @see HttpServlet#HttpServlet()
      */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().append("Served at: ").append(request.getContextPath());
-        response.getWriter().append("\nHello, from a Servlet!");
+    public Calculator() {
+        super();
+        // TODO Auto-generated constructor stub
     }
-
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doGet(request, response);
-    }
-
-    
-
+ 
+                /**
+                * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+                */
+                protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+                                {
+                        try
+                        {
+                        response.setContentType("text/html");
+                        PrintWriter out= response.getWriter();
+                        int a1= Integer.parseInt(request.getParameter("n1"));
+                        int a2= Integer.parseInt(request.getParameter("n2"));
+                        if(request.getParameter("r1")!=null)
+                        {
+                            out.println("<h1>Addition</h1>"+(a1+a2));
+                        }
+                        if(request.getParameter("r2")!=null)
+                        {
+                            out.println("<h1>Substraction</h1>"+(a1-a2));
+                        }
+                        if(request.getParameter("r3")!=null)
+                        {
+                            out.println("<h1>Multiplication</h1>"+(a1*a2));
+                        }if(request.getParameter("r1")!=null)
+                        {
+                            out.println("<h1>Division</h1>"+(a1/a2));
+                        }
+                        }
+                        catch(Exception e)
+                        {
+ 
+                        }
+                    }
+                }
+ 
 }
+
